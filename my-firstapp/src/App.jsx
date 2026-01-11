ce8c8d556e4b97808f0303053e265b61893ba0a8
himank kaushik <kaushikhimank72@gamil.com>
2026-01-11 20:19:32 +0530

    Learn Some JSX

--- my-firstapp/src/App.jsx
+++ my-firstapp/src/App.jsx
@@ -1,14 +1,39 @@
 import './App.css'
 import Header from './Header'
 function App() {
-
+   const firstname="Himank"
+  function lastname(){
+    return "Kaushik"
+  }
+  function sum(a,b){
+    return a+b;
+  }
+  function Calculator(a,b,op){
+   if(op=="+"){
+    return a+b;
+   }
+   else if (op=="-"){
+    return a-b;
+   }
+   else if (op=="*"){
+    return a*b;
+   }  
+   else{
+    return a/b;
+   }
+  }
   return (
     <>
+    <div>
+     <h1>JSX WITH CURLY BRACES</h1>
+     <h1>{firstname?firstname:"User Not Found"} {lastname()?lastname():"NO SURMAE FOUND"}</h1>
+     {sum(10,20)}      <br/>
+     {Calculator(200,10,"-")}  <br/>   
+     <input type='text' value={firstname}/>
+    </div>
     <Header/>
-      <div>{
-        <h1>HII  This is my first page</h1>
-      }</div>
-
+    
+     
     </>
 
   )
